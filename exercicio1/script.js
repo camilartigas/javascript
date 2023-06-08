ca70086ca73251fcc05f68c999dc98410915ee69
Camila <camiart@gmail.com>
2023-06-08 09:43:05 -0300

    JavaScript

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+var n1 = prompt("Digite o primeiro número");
+var n2 = prompt("Digite o segundo número");
+var n3 = prompt("Digite o terceiro número");
+var resultado = (Number(n1) + Number(n2) + Number(n3))/3;
+alert("o resultado da média é: " + resultado);
