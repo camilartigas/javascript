e482d1e70e346d77da3ba4a46067e46b26784488
Camila <camiart@gmail.com>
2023-06-12 14:57:10 -0300

    exercicio6

---
+++ exercicio6/script.js
@@ -0,0 +1,5 @@
+/* Concatenação de nomes */
+var primeiroNome = prompt("Qual é o seu primeiro nome?");
+var sobrenome = prompt("Qual é o seu sobrenome?");
+
+alert("Seu nome é: "+primeiroNome+" "+sobrenome);
