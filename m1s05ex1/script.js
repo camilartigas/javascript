f47b4fc8c9e0c755fcfadf57360b3e97c459557a
Camila <camiart@gmail.com>
2023-06-14 14:09:12 -0300

    m1s05ex1

---
+++ m1s05ex1/script.js
@@ -0,0 +1,13 @@
+var idade = 60;
+if (idade<= 12) {
+    console.log("Você é uma criança");
+}
+else if (idade<= 17){
+    console.log("Você é um adolescente");
+}
+else if (idade<= 59){
+    console.log("Você é um adulto");
+}
+else {
+    console.log("Você é um idoso");
+}
