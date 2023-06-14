78c8d6f4b8f7b09e1f7690698acfbcb60a9c80b2
Camila <camiart@gmail.com>
2023-06-14 14:34:38 -0300

    m1s05ex2

---
+++ m1s05ex2/script.js
@@ -0,0 +1,21 @@
+var color = "grey";
+
+switch (color) {
+    case "blue":
+        console.log("Azul");
+        break;
+    case "green":
+        console.log("Verde");
+        break;
+    case "black":
+        console.log("Preto");
+        break;
+    case "white":
+        console.log("Branco");
+        break;
+    case "pink":
+        console.log("Rosa");
+        break; 
+    default:
+        console.log("Não encontrei em meus dados a cor que você procura");  
+}
