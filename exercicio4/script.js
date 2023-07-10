c017567b830938323838a52397999bfe5ac961fc
Camila <camiart@gmail.com>
2023-07-10 13:54:11 -0300

    imprime console

--- exercicio4/script.js
+++ exercicio4/script.js
@@ -2,4 +2,6 @@ var aluno = Object();
 aluno.nome = prompt("Qual se nome?");
 aluno.idade = prompt("Qual sua idade?");
 
-alert(aluno.nome +" "+ aluno.idade);
+// alert(aluno.nome +" "+ aluno.idade); ou
+
+console.log(aluno.nome + ' '+aluno.idade);
